0637e56d7d9dd522d2731124ad5fef67fc0f58c9
Oskari Rauta <oskari.rauta@gmail.com>
2024-04-06 10:16:03 +0300

    merge scanner_cpp with common_cpp
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

---
+++ include/common/scanner.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <variant>
+
+namespace common {
+
+	using scanner_variant = std::variant<char *, unsigned char*, int*, unsigned int*, long*, unsigned long*, long long*, unsigned long long*, std::string*>;
+	using scanner_map = std::map<size_t, scanner_variant>;
+
+	size_t scan(const std::string& s, const scanner_map& m);
+}
