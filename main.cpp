2e5aa30a1e4f9185139c40d52ecd43c3f4a24055
Oskari Rauta <oskari.rauta@gmail.com>
2024-12-20 20:49:18 +0200

    update test code to include enumarate sample
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
+#include <map>
 #include "common.hpp"
 #include "lowercase_map.hpp"
 #include "common/storage.hpp"
 #include "common/scanner.hpp"
+#include "common/enumerate.hpp"
 
 void scanner_test() { // string scanner usage test
 
@@ -20,6 +23,13 @@ void scanner_test() { // string scanner usage test
 	std::cout << "i1 is " << i1 << " and i2 is " << i2 << " and i5 is " << i5 <<std::endl;
 
 	std::cout << "b is " << b << std::endl;
+}
+
+void enumerate_test() {
+
+	std::map<std::string, std::string> m = {{ "var1", "val1" }, { "var2", "val2" }, { "var3", "val3" }};
+	for ( const auto& [idx, elem] : common::enumerate(m))
+		std::cout << "#" << idx << ": " << elem.first << " = " << elem.second << std::endl;
 }
 
 int main(int argc, char **argv) {
