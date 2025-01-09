0fce348fb395eb7c058d9b6797494149887405bd
Oskari Rauta <oskari.rauta@gmail.com>
2025-01-09 17:50:39 +0200

    add featureset type
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

---
+++ include/featureset.hpp
@@ -0,0 +1,199 @@
+#pragma once
+
+#include <set>
+#include <algorithm>
+
+template <class T>
+class FeatureSet {
+
+	public:
+
+		template <class T2>
+		class iterator {
+
+			friend class FeatureSet<T2>;
+
+			private:
+				typename std::set<T2>::iterator it;
+				iterator(const typename std::set<T2>::iterator& it) : it(it) {}
+
+			public:
+				T2 operator *() noexcept { return *(this -> it); }
+				iterator<T2> operator++() noexcept { ++this -> it; return *this; }
+				iterator<T2> operator++(int) noexcept { iterator<T2> tmp = *this; ++this -> it; return tmp; }
+				bool operator ==(const iterator& other) const noexcept { return this -> it == other.it; }
+				bool operator !=(const iterator& other) const noexcept { return this -> it != other.it; }
+		};
+
+		bool operator ==(const T& type) const;
+		bool operator !=(const T& type) const;
+		bool operator [](const T& type) const;
+		FeatureSet<T>& operator =(const T& type);
+		FeatureSet<T>& operator ^(const T& type);
+		FeatureSet<T>& operator ^=(const T& type);
+		FeatureSet<T>& operator +=(const T& type);
+		FeatureSet<T>& operator -=(const T& type);
+
+		FeatureSet<T>& operator =(const FeatureSet<T>& other);
+		bool operator ==(const FeatureSet<T>& other);
+
+		bool contains(const T& type) const;
+		void set(const T& type, bool state = true);
+		void unset(const T& type);
+		void erase(const T& type);
+
+		void clear();
+		size_t size();
+		bool empty();
+
+		iterator<T> begin();
+		iterator<T> end();
+
+	private:
+		std::set<T> store;
+};
+
+template <class T>
+bool FeatureSet<T>::operator ==(const T& type) const {
+
+	auto it = std::find_if(this -> store.begin(), this -> store.end(), [&type](const T& t) { return type == t; });
+	return it != this -> store.end();
+}
+
+template <class T>
+bool FeatureSet<T>::operator !=(const T& type) const {
+
+	return !(this -> operator==(type));
+}
+
+template <class T>
+bool FeatureSet<T>::operator [](const T& type) const {
+
+	return this -> operator==(type);
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator =(const T& type) {
+
+	if ( bool contains = this -> operator ==(type); !contains )
+		this -> store.insert(type);
+
+	return *this;
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator ^(const T& type) {
+
+	if ( bool contains = this -> operator ==(type); contains )
+		this -> store.erase(type);
+
+	return *this;
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator ^=(const T& type) {
+
+	if ( bool contains = this -> operator ==(type); contains )
+		this -> store.erase(type);
+
+	return *this;
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator +=(const T& type) {
+
+	if ( bool contains = this -> operator ==(type); !contains )
+		this -> store.insert(type);
+
+	return *this;
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator -=(const T& type) {
+
+	if ( bool contains = this -> operator ==(type); contains )
+		this -> store.erase(type);
+
+	return *this;
+}
+
+template <class T>
+FeatureSet<T>& FeatureSet<T>::operator =(const FeatureSet<T>& other) {
+
+	this -> store.clear();
+	for ( auto it = other.store.begin(); it != other.store.end(); it++ ) {
+		T value = *it;
+		this -> store.insert(value);
+	}
+	return *this;
+}
+
+template <class T>
+bool FeatureSet<T>::operator ==(const FeatureSet<T>& other) {
+
+	if ( this -> store.size() != other.store.size())
+		return false;
+
+	for ( auto it = this -> store.begin(); it != this -> store.end(); it++ ) {
+		T value = *it;
+		if ( other != value )
+			return false;
+	}
+
+	return true;
+}
+
+template <class T>
+bool FeatureSet<T>::contains(const T& type) const {
+
+	return this -> operator==(type);
+}
+
+template <class T>
+void FeatureSet<T>::set(const T& type, bool state) {
+
+	if ( state && !this -> contains(type))
+		this -> store.insert(type);
+	else if ( !state && this -> contains(type))
+		this -> store.erase(type);
+}
+
+template <class T>
+void FeatureSet<T>::unset(const T& type) {
+
+	this -> set(type, false);
+}
+
+template <class T>
+void FeatureSet<T>::erase(const T& type) {
+
+	this -> set(type, false);
+}
+
+template <class T>
+void FeatureSet<T>::clear() {
+
+	this -> store.clear();
+}
+
+template <class T>
+size_t FeatureSet<T>::size() {
+
+	return this -> store.size();
+}
+
+template <class T>
+bool FeatureSet<T>::empty() {
+
+	return this -> store.empty();
+}
+
+template <class T>
+FeatureSet<T>::iterator<T> FeatureSet<T>::begin() {
+	return FeatureSet<T>::iterator<T>(this -> store.begin());
+}
+
+template <class T>
+FeatureSet<T>::iterator<T> FeatureSet<T>::end() {
+	return FeatureSet<T>::iterator<T>(this -> store.end());
+}
